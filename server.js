ef09fcfc7716efe5ba104d4c016144014eebc3dc
MP <47081376+mpiechulek@users.noreply.github.com>
2020-12-18 12:59:20 +0100

    7 atempt ^^

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(_dirname + '/dist/angular-to-heroku-app'));
+app.use(express.static(__dirname + '/dist/angular-to-heroku-app'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(_dirname + '/dist/angular-to-heroku-app/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/angular-to-heroku-app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
