a620ffcfbb928af0e03c3232df5e90ac215ded47
MP <47081376+mpiechulek@users.noreply.github.com>
2020-12-18 12:33:27 +0100

    5 attention to fixe bug

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ console.log('fdsfdfdsdf');
 app.use(express.static(_dirname + '/dist/angular-to-heroku-app'));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/angular-to-heroku-app/index.thml'));
+  res.sendFile(path.join(__dirname + '/dist/angular-to-heroku-app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
